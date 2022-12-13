bab073204449767c46bcbba85bb9411c687fafd8
lauwarm <10065855+lauwarm@users.noreply.github.com>
2022-12-13 14:21:25 +0100

    Initial Commit

---
+++ advent-of-code-2022/advent_of_code_2022App.swift
@@ -0,0 +1,17 @@
+//
+//  advent_of_code_2022App.swift
+//  advent-of-code-2022
+//
+//  Created by Fabian on 13.12.22.
+//
+
+import SwiftUI
+
+@main
+struct advent_of_code_2022App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
