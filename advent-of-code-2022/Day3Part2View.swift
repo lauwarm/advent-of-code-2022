f730a1afbd57f9e5932cfd51795b82321934387e
lauwarm <10065855+lauwarm@users.noreply.github.com>
2023-02-26 13:41:56 +0100

    added day 3 template

---
+++ advent-of-code-2022/Day3Part2View.swift
@@ -0,0 +1,20 @@
+//
+//  Day3Part2View.swift
+//  advent-of-code-2022
+//
+//  Created by Fabian on 26.02.23.
+//
+
+import SwiftUI
+
+struct Day3Part2View: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Day3Part2View_Previews: PreviewProvider {
+    static var previews: some View {
+        Day3Part2View()
+    }
+}
---
+++ advent-of-code-2022/Day3Solver.swift
@@ -0,0 +1,8 @@
+//
+//  Day3Solver.swift
+//  advent-of-code-2022
+//
+//  Created by Fabian on 26.02.23.
+//
+
+import Foundation
