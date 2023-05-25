103b35fcae0b728eafa9dc2e464c12329e1e3d88
lauwarm <10065855+lauwarm@users.noreply.github.com>
2023-05-25 11:41:50 +0200

    added day 8 solver template

--- advent-of-code-2022/Day8/Day8Solver.swift
+++ advent-of-code-2022/Day8/Day8Solver.swift
@@ -6,3 +6,15 @@
 //
 
 import Foundation
+
+func day8part1solution(input: String)->Int {
+    var solution = 0
+    
+    return solution
+}
+
+func day8part2solution(input: String)->Int {
+    var solution = 0
+    
+    return solution
+}
