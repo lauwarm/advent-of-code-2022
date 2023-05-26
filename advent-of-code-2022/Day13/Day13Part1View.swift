698dcc0b6c07cdc7c466d4a193d5d941d49e8a5c
lauwarm <10065855+lauwarm@users.noreply.github.com>
2023-05-26 11:46:16 +0200

    added day 13 swiftui file

---
+++ advent-of-code-2022/Day13/Day13Part1View.swift
@@ -0,0 +1,20 @@
+//
+//  Day13Part1View.swift
+//  advent-of-code-2022
+//
+//  Created by Fabian on 26.05.23.
+//
+
+import SwiftUI
+
+struct Day13Part1View: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Day13Part1View_Previews: PreviewProvider {
+    static var previews: some View {
+        Day13Part1View()
+    }
+}
---
+++ advent-of-code-2022/Day13/Day13Part2View.swift
@@ -0,0 +1,20 @@
+//
+//  Day13Part2View.swift
+//  advent-of-code-2022
+//
+//  Created by Fabian on 26.05.23.
+//
+
+import SwiftUI
+
+struct Day13Part2View: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Day13Part2View_Previews: PreviewProvider {
+    static var previews: some View {
+        Day13Part2View()
+    }
+}
