8fcc85ac7a65818aabf5b3036a3dde01377f1bfb
lauwarm <10065855+lauwarm@users.noreply.github.com>
2023-05-26 11:45:49 +0200

    added day 12 swiftui file

---
+++ advent-of-code-2022/Day12/Day12Part2View.swift
@@ -0,0 +1,20 @@
+//
+//  Day12Part2View.swift
+//  advent-of-code-2022
+//
+//  Created by Fabian on 26.05.23.
+//
+
+import SwiftUI
+
+struct Day12Part2View: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Day12Part2View_Previews: PreviewProvider {
+    static var previews: some View {
+        Day12Part2View()
+    }
+}
