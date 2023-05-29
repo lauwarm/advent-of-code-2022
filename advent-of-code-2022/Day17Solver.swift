6a87608854cb1c7d5e8f42b69c5950293c6a662d
lauwarm <10065855+lauwarm@users.noreply.github.com>
2023-05-29 16:32:14 +0200

    added day 17 solver template

--- advent-of-code-2022/Day17Solver.swift
+++ advent-of-code-2022/Day17Solver.swift
@@ -6,3 +6,15 @@
 //
 
 import Foundation
+
+func day17part1solution(input: String)->Int {
+    var solution = 0
+    
+    return solution
+}
+
+func day17part2solution(input: String)->Int {
+    var solution = 0
+    
+    return solution
+}
