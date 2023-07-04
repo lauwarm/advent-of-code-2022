4d2312def6b21e1bb8917b667a0ba022fb0563a7
lauwarm <10065855+lauwarm@users.noreply.github.com>
2023-07-04 21:21:10 +0200

    added day 4 part 2 solution

--- advent-of-code-2022/Day4/Day4Solver.swift
+++ advent-of-code-2022/Day4/Day4Solver.swift
@@ -22,7 +22,6 @@ func day4part1solution(input: String)->Int {
             print("endline")
         }
         else {
-            //print(line.components(separatedBy: ",")[0].components(separatedBy: "-")[0])
             start11 = Int(line.components(separatedBy: ",")[0].components(separatedBy: "-")[0]) ?? 0
             start12 = Int(line.components(separatedBy: ",")[0].components(separatedBy: "-")[1]) ?? 0
             start21 = Int(line.components(separatedBy: ",")[1].components(separatedBy: "-")[0]) ?? 0
@@ -50,6 +49,33 @@ func day4part1solution(input: String)->Int {
 
 func day4part2solution(input: String)->Int {
     var solution = 0
+    
+    let lines = input.components(separatedBy: "\n")
+
+    var start1 = 0
+    var ende1 = 0
+    var start2 = 0
+    var ende2 = 0
+    
+    for line in lines {
+        if (line.count == 0) {
+            print("endline")
+        }
+        else {
+            start1 = Int(line.components(separatedBy: ",")[0].components(separatedBy: "-")[0]) ?? 0
+            ende1 = Int(line.components(separatedBy: ",")[0].components(separatedBy: "-")[1]) ?? 0
+            start2 = Int(line.components(separatedBy: ",")[1].components(separatedBy: "-")[0]) ?? 0
+            ende2 = Int(line.components(separatedBy: ",")[1].components(separatedBy: "-")[1]) ?? 0
+            
+            if (ende1 < start2 || start2 > ende1 || start1 > ende2) {
+                print("yes")
+            } else {
+                print("no")
+                solution += 1
+            }
+        }
+        print(solution)
+    }
     
     return solution
 }
