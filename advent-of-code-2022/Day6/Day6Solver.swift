6d9a36373ac1bd7b613b0a78ceb722cb9552433b
lauwarm <10065855+lauwarm@users.noreply.github.com>
2023-07-13 08:35:37 +0200

    update day 6; added test cases

--- advent-of-code-2022/Day6/Day6Solver.swift
+++ advent-of-code-2022/Day6/Day6Solver.swift
@@ -10,6 +10,21 @@ import Foundation
 func day6part1solution(input: String)->Int {
     var solution = 0
     
+    let lines = input.components(separatedBy: "\n")
+
+    // test data and solution
+    let test1 = "mjqjpqmgbljsphdztnvjfqwrcgsmlb"
+    let test2 = "bvwbjplbgvbhsrlpgdmjqwftvncz"
+    let test3 = "nppdvjthqldpwncqszvftbrmjlhg"
+    let test4 = "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"
+    let test5 = "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"
+    let ergebnis1 = 7
+    let ergebnis2 = 5
+    let ergebnis3 = 6
+    let ergebnis4 = 10
+    let ergebnis5 = 11
+    
+    
     return solution
 }
 
